9332c65241ad6738b90129088b371a738431f5df
ryandang <Dang@sisryandang.local>
2017-05-15 17:28:04 +0900

    add moment ja

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 export default class App extends React.Component {
     render() {
+        var moment = require('moment');
+        moment.locale('ja');
+
+        console.log(moment().format());
+
         return (
             <div style={{textAlign: 'center'}}>
-                <h1>Hello World</h1>
+                <h1>今日は{moment().format('dddd')}です。</h1>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-var moment = require('moment');
-console.log(moment().format());
-
 ReactDOM.render(
     <App />,
     document.getElementById('app')
