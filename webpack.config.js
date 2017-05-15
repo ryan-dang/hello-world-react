03d8e655bce7bf6ffa655994fb732fcc0f9ff2f0
ryandang <Dang@sisryandang.local>
2017-05-15 16:34:11 +0900

    reduce variables

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,6 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './src/index.html',
-    filename: 'index.html',
-    inject: 'body'
-})
-
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -20,5 +14,11 @@ module.exports = {
             { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
         ]
     },
-    plugins: [HtmlWebpackPluginConfig]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
 }
