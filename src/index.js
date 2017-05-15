0818384c1a46ab33fe9467f731ff8a1da448a33c
ryandang <Dang@sisryandang.local>
2017-05-15 17:01:53 +0900

    add webpack code splitting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
+var moment = require('moment');
+console.log(moment().format());
+
 ReactDOM.render(
     <App />,
     document.getElementById('app')
