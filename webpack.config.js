96bb340a2760948537f3947f99446dddbca88e5e
ryandang <Dang@sisryandang.local>
2017-05-15 16:37:04 +0900

    same naming as webpack sample

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.join(__dirname, "build"),
+        path: path.join(__dirname, "dist"),
         filename: 'app.js'
     },
     module: {
