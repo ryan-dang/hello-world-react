6e772d072091eb0f8b2b72aacc553bebc128dd6d
ryandang <Dang@sisryandang.local>
2017-05-16 13:25:57 +0900

    reorganize webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
 module.exports = (env = {}) => {
-  return require(`./webpack.${env}.js`)
+
+    try {
+        if (['dev', 'prod'].indexOf(env) === -1) {
+            throw new Error('\'' + env + "' is not valid env flag.  Please pass '--env dev' or '--env prod'.")
+        }
+    } catch (e) {
+        return console.error(e)
+    }
+
+    return require('./webpack/' + env + '.js')
 }
